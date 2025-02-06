551011bba13f084fd6a407a1aea936dd1e20cd19
mj <mjmartinsu@yahoo.es>
2025-02-06 13:47:57 +0100

    Extent with controlls parameters

---
+++ src/controllers/user.controllers.ts
@@ -0,0 +1,5 @@
+export const a = () => {};
+export const b = () => {};
+export const c = () => {};
+export const d = () => {};
+export const e = () => {};
---
+++ src/routers/user.routers.ts
@@ -0,0 +1,4 @@
+import { Router } from 'expres';
+import { a, b, c, d, e } from './controller.js';
+
+userRouters.get('/');
